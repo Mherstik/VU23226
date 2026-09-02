a064a6539d3b45dce49f6ffcf2571478e3edabe7
Marcus Herstik <marcus@herstik.com>
2026-09-02 17:44:58 +1000

    Update dashboard

--- db.php
+++ db.php
@@ -4,11 +4,19 @@ $dbname = "login_system";
 $dbuser = "login_app"; 
 $dbpass = "strongpass"; 
 try { 
-	$pdo = new PDO( "mysql:host=$host;dbname=$dbname;charset=utf8mb4", 
-	$dbuser, 
-	$dbpass, 
-	[ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ] ); 
-	} 
-catch (PDOException $e) { die("Database connection failed."); 
-	} 
-?>
+	$pdo = new PDO( 
+		"mysql:host=$host;dbname=$dbname;charset=utf8mb4", 
+		$dbuser, 
+		$dbpass, 
+		[ 
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC 
+			 PDO::ATTR_EMULATE_PREPARES => false
+        ]
+    );
+
+} catch (PDOException $e) {
+
+    die("Database connection failed.");
+
+}
