4e32d43cce0f65442e210f935f59212f352a9419
Marcus Herstik <marcus@herstik.com>
2026-09-02 17:18:26 +1000

    test database

--- db.php
+++ db.php
@@ -9,7 +9,6 @@ try {
 	$dbpass, 
 	[ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ] ); 
 	} 
-catch (PDOException $e) { 
-	die("Database connection failed."); 
+catch (PDOException $e) { die("Database connection failed."); 
 	} 
 ?>
